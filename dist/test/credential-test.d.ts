3b680e157f37e7533aad0433a07ff3ccf3b4bb1e
Paul Chin Jr <pchinjr@gmail.com>
2025-07-10 16:10:16 -0400

    Remove /dist from git tracking - build artifacts should not be versioned
    - Compiled TypeScript files are generated by 'npm run build'
    - /dist already properly excluded in .gitignore
    - Reduces repository size and prevents merge conflicts on generated files
    - Local /dist directory preserved for development use

--- dist/test/credential-test.d.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ts-node
-declare function testCredentialSeparation(): Promise<void>;
-export { testCredentialSeparation };
-//# sourceMappingURL=credential-test.d.ts.map
--- dist/test/permission-test.d.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ts-node
-declare function testPermissionManagement(): Promise<void>;
-export { testPermissionManagement };
-//# sourceMappingURL=permission-test.d.ts.map
