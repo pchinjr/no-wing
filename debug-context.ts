8aedc24d5717653acbcc87051f0253e018bfb1e8
Paul Chin Jr <pchinjr@gmail.com>
2025-07-21 16:30:13 -0400

    chore: bulldoze and try again

--- debug-context.ts
+++
@@ -1,6 +0,0 @@
-import { ContextManager } from './src/config/ContextManager.ts';
-
-console.log('Current working directory:', Deno.cwd());
-const contextManager = new ContextManager();
-const context = contextManager.detectContext();
-console.log('Detected context:', context);
